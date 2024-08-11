070ad5e92864ef8c2126838f5c99d8ccef46be76
TechaltBIG <tech.altbig@gmail.com>
2024-08-11 20:14:26 +0530

    Update Navbar.jsx

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -55,7 +55,7 @@ const Navbar = () => {
     <>
       <header className="header">
         <a href="#" className="logo">
-          <img className="logo-stl" src="../../src/assets/TSLlogonew.png" />
+          <img className="logo-stl" src="https://the-salt-legal-git-main-tech-altbigs-projects.vercel.app/assets/TSLlogonew.png" />
         </a>
         <input className="menu-btn" type="checkbox" id="menu-btn" />
         <label className="menu-icon" for="menu-btn">
